ff12f3cdf7c0bda10dd8f639a47760d6f67f6fed
sobujra010 <dreamer.sobuj.010@gmail.com>
2020-03-04 16:36:55 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undifined.js
@@ -0,0 +1,16 @@
+// let pakhi;
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+
+// const marks = {bangla : 50, english : 60, };
+// console.log(marks.history);
+
+// let set = undefined;
+// console.log(set);
+
