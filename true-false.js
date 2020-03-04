5b08281e026bbdef6fc4a68bf3407798282df140
sobujra010 <dreamer.sobuj.010@gmail.com>
2020-03-04 16:10:24 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// Falsy:
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+// Trithy :
+// '0', " " []
+
+const age = 0;
+
+if(age){
+    console.log("condition is true.");
+}
+else{
+    console.log("condition is false.");
+}
